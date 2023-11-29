3f3dddf4495b84a4d6aeb55bf70ea29c1a1b47d5
Isabella Cavalcante <108815118+IsabellaMyreah@users.noreply.github.com>
2023-11-29 16:51:15 -0300

    Add files via upload

---
+++ businesSystem/js/script.js
@@ -0,0 +1,29 @@
+function acao(){
+
+  let modal = document.querySelector('.modalLogin');
+
+  modal.style.display = 'block';
+}
+
+function fechar(){
+
+  let modal = document.querySelector('.modalLogin');
+
+  modal.style.display = 'none';
+
+}
+
+function acaoTwo(){
+
+  let modalCad = document.querySelector('.modalCadastro');
+
+  modalCad.style.display = 'block';
+}
+
+function fecharTwo(){
+
+  let modalCad = document.querySelector('.modalCadastro');
+
+  modalCad.style.display = 'none';
+
+}
